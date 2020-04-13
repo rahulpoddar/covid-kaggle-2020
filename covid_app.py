bc582f840c5459499331d43b572e17554dbf0fbe
rahulpoddar <rahul.pod@gmail.com>
2020-04-13 10:57:24 +0530

    Add files via upload

---
+++ covid_app.py
@@ -0,0 +1,73 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import pandas as pd
+import dash_table
+from dash.dependencies import Input, Output, State
+
+df = pd.read_excel('./TASK1_annotated_1.xlsx', sheet_name = 'Newone')
+
+tasks = df['Kaggle Task name'].unique().tolist()
+
+def generate_summary(task):
+    return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+def generate_table(dff):
+    rel_cols = ['Document id_', 'Output']
+    return dash_table.DataTable(
+            id = 'search-results-table',
+            columns = [{"name": i, "id": i} for i in dff[rel_cols].columns],
+            data = dff[rel_cols].to_dict('records'),
+            style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto'
+    },
+        style_cell={'textAlign': 'left'},
+        style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+    },
+            )
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+        html.Div([
+        html.H1('COVID-19 Open Research Dataset Challenge (CORD-19)'),
+        dcc.Dropdown(
+        id='task-dropdown',
+        options=[
+            {'label': i, 'value': i} for i in tasks 
+        ],
+        placeholder="Select a task",
+    )]),
+    
+    html.Div([html.H3('Response Summary', id = 'task-summary-heading'),
+    html.Div(id = 'task-summary')]),
+    
+    html.Div([
+            html.H3('Search Results'),
+            html.Div(id = 'search-results')
+            ])
+])
+
+
+@app.callback(
+    dash.dependencies.Output('task-summary', 'children'),
+    [dash.dependencies.Input('task-dropdown', 'value')])
+def update_summary(value):
+    return generate_summary(value)
+
+@app.callback(
+    dash.dependencies.Output('search-results', 'children'),
+    [dash.dependencies.Input('task-dropdown', 'value')])
+def update_search_results(value):
+    dff = df[df['Kaggle Task name'] == value]
+    return generate_table(dff)
+    
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
